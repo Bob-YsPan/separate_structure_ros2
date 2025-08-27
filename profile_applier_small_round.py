e68fef413e671505c4b6c6bb1022c0642561fbe8
Bob-YsPan <s96116s@gmail.com>
2025-08-27 23:50:32 +0800

    Flip lidar

--- profile_applier_small_round.py
+++ profile_applier_small_round.py
@@ -17,12 +17,12 @@ class Profile_params():
     robot_size_y = 0.190
     robot_size_z = 0.110
     # Laser position (Robot body's center --> Laser)
-    laser_pos_x = -0.065
+    laser_pos_x = 0.065
     laser_pos_y = 0.0
     laser_pos_z = 0.105
     laser_pos_r = 0.0
     laser_pos_p = 0.0
-    laser_pos_yw = 0.0
+    laser_pos_yw = 3.14159
     # IMU position (Robot body's center --> IMU)
     imu_pos_x = 0.065
     imu_pos_y = -0.055
